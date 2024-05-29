541926aafa59ae5a22fa18f8c132caa22f53c453
Rob Pilling <robpilling@gmail.com>
2024-05-29 21:39:44 +0100

    Add Overlay trait

---
+++ overlay/src/lib.rs
@@ -0,0 +1,11 @@
+#![no_std]
+
+pub trait Overlay {
+    fn overlay(bytes: &[u8]) -> Result<&Self, Error>;
+    fn overlay_mut(bytes: &mut [u8]) -> Result<&mut Self, Error>;
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub enum Error {
+    InsufficientLength,
+}
