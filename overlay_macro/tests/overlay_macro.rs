680aa9e799bd175292f785debd2548296521ad93
Rob Pilling <robpilling@gmail.com>
2024-05-30 18:00:37 +0100

    Conditionally implement Debug by extracting from #[derive(...)]

--- overlay_macro/tests/overlay_macro.rs
+++ overlay_macro/tests/overlay_macro.rs
@@ -2,7 +2,7 @@ use overlay::Overlay;
 use overlay_macro::overlay;
 
 #[overlay]
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct InquiryCommand {
     #[bit_byte(7, 0, 0, 0)]
     pub op_code: u8,
@@ -15,6 +15,13 @@ pub struct InquiryCommand {
 
     #[bit_byte(13, 0, 3, 4)]
     pub allocation_length: u16,
+}
+
+#[overlay]
+#[derive(Clone)]
+pub struct NoDebug {
+    #[bit_byte(7, 0, 0, 0)]
+    pub op_code: u8,
 }
 
 #[test]
@@ -52,4 +59,25 @@ fn setters() {
 
     inq.set_allocation_length(1281);
     assert_eq!(inq.0, [5, 5, 1 | (23 << 1), 5, 1]);
+}
+
+#[test]
+fn debug() {
+    let mut bytes = [
+        5_u8,
+        5, // true, 1<<4 is ignored
+        1 | (3 << 1),
+        1,
+        4, // 1 << 8 | 4, i.e. 260
+    ];
+    let inq = InquiryCommand::overlay_mut(&mut bytes).unwrap();
+
+    let mut out = String::new();
+    use std::fmt::Write;
+    write!(&mut out, "{inq:?}").unwrap();
+
+    assert_eq!(
+        &out,
+        "InquiryCommand { op_code: 5, product_data: true, page_code: 3, allocation_length: 260 }"
+    );
 }
